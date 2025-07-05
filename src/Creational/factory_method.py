92f6f7e2d1d3432d9cd7c9aa90e7baffa97720de
alena-alexander <alena.alexander04@gmail.com>
2025-07-05 06:32:00 -0400

    innit commit

--- src/Creational/factory_method.py
+++ src/Creational/factory_method.py
@@ -1,7 +1,3 @@
-"""
-
-"""
-
 from abc import ABC, abstractmethod
 
 
@@ -39,9 +35,19 @@ class SkullShirt(ShirtFactory):
         self._material = "Cotton"
 
     def shirt_color(self):
+        """
+        Must display the color of the skull shirt.
+
+        :return: str
+        """
         return self._color
 
     def shirt_material(self):
+        """
+        Must display the skull shirts material.
+
+        :return: str
+        """
         return self._material
 
 
@@ -51,9 +57,61 @@ class FlowerShirt(ShirtFactory):
         self._material = "Silk"
 
     def shirt_color(self):
+        """
+        Must display the color of the flower shirt.
+
+        :return: str
+        """
         return self._color
 
     def shirt_material(self):
+        """
+        Must display the flower shirts material.
+
+        :return: str
+        """
         return self._material
 
 
+def main():
+    """
+    As long as this while loop is True the program will ask the user to "Choose a shirt between the Skull shirt,
+    and Flower shirt, to identify "the materials of it" and if the shirt chosen is within the two options given then
+    the loop will break, ignore the else statement and move on to the code below.
+
+    :return: str
+    """
+    while True:
+        shirt_chosen = input("Choose a shirt between the Skull shirt, and Flower shirt, to identify "
+                             "the materials of it")
+
+        if shirt_chosen in ["Skull shirt", "Flower shirt"]:
+            break
+        else:
+            print("invalid option was chosen...")
+
+    """
+    The code below identifies the variables skull_shirt and flower_shirt as type SkullShirt, and FlowerShirt, allowing
+    you to use those classes methods and such. And if the shirt chosen by the user is "Skull shirt", then an instance of
+    the SkullShirt class is created, else if the shirt chosen by the user is "Flower shirt", then an instance of
+    the FlowerShirt class is created. And then then the methods for the classes are ran.
+    """
+    skull_shirt: SkullShirt
+    flower_shirt: FlowerShirt
+
+    if shirt_chosen == "Skull shirt":
+        skull_shirt = SkullShirt()
+    elif shirt_chosen == "Flower shirt":
+        flower_shirt = FlowerShirt()
+    else:
+        print("Meh...")
+
+    skull_shirt.shirt_material()
+    flower_shirt.shirt_material()
+
+    skull_shirt.shirt_color()
+    flower_shirt.shirt_color()
+
+
+if __name__ == "__main__":
+    main()
